995cc8574323094484dd38f726996cc1efc3a0e2
Cmagana6 <0006106@uca.edu.sv>
2018-07-10 10:24:58 -0600

    AlbumDAO añadido

--- app/src/main/java/com/luisro00005513/pdmparcial3/Database/DAO/AlbumDAO.java
+++ app/src/main/java/com/luisro00005513/pdmparcial3/Database/DAO/AlbumDAO.java
@@ -1,6 +1,14 @@
 package com.luisro00005513.pdmparcial3.Database.DAO;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.luisro00005513.pdmparcial3.Database.Entities.AlbumDB;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by UCA on 10/07/2018.
@@ -8,4 +16,16 @@ import android.arch.persistence.room.Dao;
 
 @Dao
 public interface AlbumDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAlbumList(List<AlbumDB> albumList);
+
+    @Query("SELECT * FROM AlbumDB WHERE game = :gamename")
+    List<AlbumDB> getAlbumbyGame(String gamename);
+
+    @Query("SELECT * FROM AlbumDB WHERE albumname = :albumname")
+    List<AlbumDB> getAlbumbyName(String albumname);
+
+    @Query("SELECT * FROM AlbumDB WHERE IdAlbum = :idalbum")
+    List<AlbumDB> getAlbumbyId (String idalbum);
+
 }
