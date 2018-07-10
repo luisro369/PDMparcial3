12146e4c216ca43bcbcc1c45895ffa30daf4bf9e
Cmagana6 <0006106@uca.edu.sv>
2018-07-10 11:02:56 -0600

    Merge branch 'branch_bases' of C:\Users\USUARIO\Documents\Programacion de dispositivos moviles\Tareas de moviles\Parcial Final\PDMparcial3 with conflicts.

--- app/src/main/java/com/luisro00005513/pdmparcial3/Database/DAO/AlbumDAO.java
+++ app/src/main/java/com/luisro00005513/pdmparcial3/Database/DAO/AlbumDAO.java
@@ -1,17 +1,17 @@
 package com.luisro00005513.pdmparcial3.Database.DAO;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
-import android.arch.persistence.room.Update;
 
 import com.luisro00005513.pdmparcial3.Database.Entities.AlbumDB;
-import com.luisro00005513.pdmparcial3.Database.Entities.CardDB;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.Flowable;
+import io.reactivex.Single;
 
 /**
  * Created by UCA on 10/07/2018.
@@ -19,29 +19,20 @@ import java.util.List;
 
 @Dao
 public interface AlbumDAO {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insert(AlbumDB... albumDB);
-
-    @Query("DELETE FROM albumDB")
-    void DeleteAllAlbum();
-
-    @Delete
-    void delete(AlbumDB albumDB);
-
-    @Update
-    void update(AlbumDB... albumDB);
-
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    void insertAlbumList(List<AlbumDB> albumList);
+    void insertAlbums(List<AlbumDB> albums);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAlbums(AlbumDB... albums);
 
     @Query("SELECT * FROM AlbumDB WHERE game = :gamename")
-    List<AlbumDB> getAlbumbyGame(String gamename);
+    Flowable<List<AlbumDB>> getAlbumbyGame(String gamename);
 
     @Query("SELECT * FROM AlbumDB WHERE albumname = :albumname")
-    List<AlbumDB> getAlbumbyName(String albumname);
+    Single<AlbumDB> getAlbumbyName(String albumname);
 
     @Query("SELECT * FROM AlbumDB WHERE IdAlbum = :idalbum")
-    List<AlbumDB> getAlbumbyId (String idalbum);
+    Single<AlbumDB> getAlbumbyId (String idalbum);
 
 }
