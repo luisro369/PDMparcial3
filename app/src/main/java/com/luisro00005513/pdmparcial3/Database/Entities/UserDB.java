5c8bf515f64be59d7f009da8c14d28573c8b5083
TheAlexBig <00062816@uca.edu.sv>
2018-07-10 09:03:28 -0600

    Actualizacion de entidades.

--- app/src/main/java/com/luisro00005513/pdmparcial3/Database/Entities/UserDB.java
+++ app/src/main/java/com/luisro00005513/pdmparcial3/Database/Entities/UserDB.java
@@ -12,8 +12,76 @@ import android.support.annotation.NonNull;
 public class UserDB {
     @NonNull
     @PrimaryKey
-    @ColumnInfo (name = "_id")
+    @ColumnInfo (name = "Id")
     private long Id;
-    @ColumnInfo (name = "username")
+    @ColumnInfo (name = "Username")
     private String Username;
+    @ColumnInfo (name = "Password")
+    private String Password;
+    @ColumnInfo(name = "FirstName")
+    private String FirstName;
+    @ColumnInfo (name = "LastName")
+    private String LastName;
+    @ColumnInfo (name = "PhoneNumber")
+    private String PhoneNumber;
+    @ColumnInfo (name = "Email")
+    private String Email;
+
+    @NonNull
+    public long getId() {
+        return Id;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setId(@NonNull long id) {
+        Id = id;
+    }
+
+    public void setUsername(String username) {
+        Username = username;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
