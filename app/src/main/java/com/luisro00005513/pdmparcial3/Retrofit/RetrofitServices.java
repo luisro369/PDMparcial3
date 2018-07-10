34119e4de26aa36655476d1bae7bac7ca310be9b
luisro369 <00005513@uca.edu.sv>
2018-07-10 13:33:18 -0600

    borrando appRepository

--- app/src/main/java/com/luisro00005513/pdmparcial3/Retrofit/RetrofitServices.java
+++ app/src/main/java/com/luisro00005513/pdmparcial3/Retrofit/RetrofitServices.java
@@ -1,5 +1,10 @@
 package com.luisro00005513.pdmparcial3.Retrofit;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,11 +15,20 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RetrofitServices{
+    public static String token;
     public static final String BASE_URL = "https://xover-api.herokuapp.com";
 
     public RetrofitService conectar(){
         //--------------conexion retrofit--------------------
-
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request newRequest = chain.request().newBuilder()
+                        .addHeader("Authorization","Bearer " + token)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
 
         Retrofit.Builder buider = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
