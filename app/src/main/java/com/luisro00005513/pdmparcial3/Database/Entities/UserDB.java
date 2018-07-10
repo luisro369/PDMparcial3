a0b38b54d72743b1ba2062e108f7f5fbd9fa7305
TheAlexBig <00062816@uca.edu.sv>
2018-07-10 08:39:07 -0600

    Actualizacion de gradle.

--- app/src/main/java/com/luisro00005513/pdmparcial3/Database/Entities/UserDB.java
+++ app/src/main/java/com/luisro00005513/pdmparcial3/Database/Entities/UserDB.java
@@ -1,8 +1,19 @@
 package com.luisro00005513.pdmparcial3.Database.Entities;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
 /**
  * Created by UCA on 10/07/2018.
  */
-
+@Entity
 public class UserDB {
+    @NonNull
+    @PrimaryKey
+    @ColumnInfo (name = "_id")
+    private long Id;
+    @ColumnInfo (name = "username")
+    private String Username;
 }
